7fa821c585a2906af193b99aa574b6d5840a184a
yohannesata <yohannesataklti9@gmail.com>
2022-09-14 15:05:22 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+
+/**
+ *
+ * main - print alphabetical order in lowercase
+ *
+ *
+ *
+ * Return: Always 0.
+ *
+ */
+void print_alphabet(void)
+
+{
+	int x;
+
+	for (x =  'a'; j <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
