df9f47f3598943b3ddab65439a0e4b67802fdb12
yohannesata <yohannesataklti9@gmail.com>
2022-09-16 12:37:48 -0700

    fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,12 +5,16 @@
  * _isupper - checks for uppercase.
  *@c: character being tested
  * Return: 1 if true else 0.
- */
+*/
 
-int _isupper(int c)
+int _isupper(int c)8
 {
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	{
 	else
+	{
 		return (0);
+	}
 }
