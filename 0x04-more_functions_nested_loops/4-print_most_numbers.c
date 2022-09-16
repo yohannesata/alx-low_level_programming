75d39088bd1f80670bd2f28b7a63b35cf26ea207
yohannesata <yohannesataklti9@gmail.com>
2022-09-16 14:44:03 -0700

    most

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/*print\-most_numbers - prints only selected numbers
+ *
+ * Returns 1 upto 9 numbers except 2 and 4
+ */
+
+void print_most_numbers(void);
+{
+	int c = 0;
+
+	for (; c <=9; c++)
+	{
+		if (c==2 || c==4)
+		{
+			continue;
+		}
+		else
+		{
+		_putchar(c + '0');
+		}
+	}
+	_putchar('\n');
+}
