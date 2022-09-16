9fdcaf9e98761b967f0e1675d08d9f63070d056d
yohannesata <yohannesataklti9@gmail.com>
2022-09-16 13:54:20 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main"
+
+/*
+ * mal - multiplies two numbers
+ * @a: first number to multiply
+ * @b: second number to multiply
+ *
+ * Return a * b
+ */
+int mul(int a, int b)
+{
+	return(a*b);
+}
