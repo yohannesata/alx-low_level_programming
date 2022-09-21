a4031e947353d8ccc7b8915f6192538545c703de
yohannesata <yohannesataklti9@gmail.com>
2022-09-21 08:45:16 -0700

    string comparison

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_strcmp - strings comparison
+ *@s1:  pointer
+ *@s2:  pointer
+ *Return: b-integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a, b;
+
+	b = 0;
+
+	for (a = 0; s1[a] != '\0' && s2[a] != '\0'; a++)
+	{
+		if (s1[a] != s2[a])
+	{
+		b = s1[a] - s2[a];
+		break;
+	}
+	}
+		return (b);
+}
