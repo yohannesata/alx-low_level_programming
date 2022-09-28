2072f3437fa03f0a90c8454c83c8b96d31b0494a
yohannesata <yohannesataklti9@gmail.com>
2022-09-28 13:40:31 -0700

    reverse

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,11 +3,12 @@
 /**
  * _strlen_recursion - returns lenght of string
  * @s: lenght of string to be returned.
- */
+ * Return: lenght of string.
+ * */
 
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-	return 0;
+	return (0);
 	return (_strlen_recursion(s + 1) + 1);
 }
