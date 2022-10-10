4e7d6cd7bb519e3e06aa892ae0f942fc978bb34c
yohannesata <yohannesataklti9@gmail.com>
2022-10-10 08:33:35 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#define DOG_H
+
+
+/**
+ * dog - data type of defining dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+
+struct dog
+{
+	char *name;
+	int age;
+	char *owner;
+};
