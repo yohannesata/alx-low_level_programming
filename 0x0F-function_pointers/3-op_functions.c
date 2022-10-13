324c04e447b791057aaff174aa519cf5b622fcfc
yohannesata <yohannesataklti9@gmail.com>
2022-10-13 10:26:46 -0700

    operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int op_sub(int a, int b);
+
+/**
+ * op_add - adds sum of two numbers
+ * @a: first number to be added
+ * @b: second number to be added
+ */
+
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - devides two numbers
+ * @a: first number
+ * @b: second number
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
